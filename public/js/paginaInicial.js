81614249cab2f296d452c7266c000b71102ea329
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2025-04-25 17:06:09 -0300

    Separando script do swiper no arquivo paginaInicial.js

---
+++ public/js/paginaInicial.js
@@ -0,0 +1,12 @@
+window.onload = function Carrossel() {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        direction: "vertical",
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        mousewheel: true,
+    });
+};
