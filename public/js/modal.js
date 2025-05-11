38d519a96071a7241dc70c9fae4e649b05053ccf
Mickaell-Oliveira <mickaell.oliveira@estudante.ufjf.br>
2025-05-10 22:14:43 -0300

    Modal editar pronto + finalização modal visualizar+ responsividade modal visualizar + js para salvar as edições

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,14 +1,62 @@
-function abrirModal(idFundo, idModal){
+function abrirModal(idFundo, idModal){ // Abre o modal
     document.getElementById(idFundo).style.display="flex";
     document.getElementById(idModal).style.display = "flex";
     setTimeout(() => {
     document.getElementById(idFundo).classList.add('opacidade'); 
     }, 10);
 }
-function fecharModal(idFundo, idModal){
+function fecharModal(idFundo, idModal){ // Fecha o modal
     document.getElementById(idFundo).classList.remove('opacidade');
     setTimeout(() => {
         document.getElementById(idFundo).style.display = 'none';
         document.getElementById(idModal).style.display = 'none';
     }, 200);
 }
+function salvarAlteracoes() {
+    // Valores inseridos no input
+    const titulo = document.getElementById('tituloPublicacaoEditar').value;
+    const autor = document.getElementById('inputAutor').value;
+    const data = document.getElementById('inputData').value;
+    const local= document.getElementById('inputLocal').value;
+    const descricao = document.getElementById('inputDescricao').value;
+    const arte = document.getElementById('previewArte').src;
+    const tag = document.getElementById('previewTag').src;
+
+    // Atualiza para os novos valores inseridos
+    document.getElementById('tituloPublicacaoEditar').textContent = titulo;
+    document.getElementById('inputAutor').textContent = autor;
+    document.getElementById('inputData').textContent = data;
+    document.getElementById('inputLocal').textContent = local;
+    document.getElementById('inputDescricao').textContent = descricao;
+    document.getElementById('previewArte').src = arte;
+    document.getElementById('previewTag').src=tag;
+
+    alert('Alterações salvas com sucesso!'); // Mensagem de sucesso
+
+    fecharModal('fundoEditar','modalEditar'); // Fecha o modal
+}
+document.addEventListener('DOMContentLoaded', function() {
+    const inputArte = document.getElementById('inputArte');
+    const labelArte = document.getElementById('labelArte');
+    const previewArte = document.getElementById('previewArte');
+
+    inputArte.addEventListener('change', function(e) {
+        const file = e.target.files[0];
+        
+        if (file) {
+            const reader = new FileReader();
+            
+            reader.onload = function(event) {
+                previewArte.src = event.target.result;
+                previewArte.style.display = 'flex';
+            };
+            labelArte.textContent='imagem selecionada'
+            
+            reader.readAsDataURL(file);
+        }else{
+            previewArte.style.display='none';
+            previewArte.src='';
+        }
+    });
+});
+
