da11939bbaccab3c285980ebe76e9c6e55748196
Mickaell-Oliveira <mickaell.oliveira@estudante.ufjf.br>
2025-05-12 22:17:32 -0300

    Finalização das responsividades +  remoção de codigo desnecessário+ pequenos ajustes

--- public/js/modal.js
+++ public/js/modal.js
@@ -35,28 +35,4 @@ function salvarAlteracoes() {
 
     fecharModal('fundoEditar','modalEditar'); // Fecha o modal
 }
-// document.addEventListener('DOMContentLoaded', function() {
-//     const inputArte = document.getElementById('inputArte');
-//     const labelArte = document.getElementById('labelArte');
-//     const previewArte = document.getElementById('previewArte');
-
-//     inputArte.addEventListener('change', function(e) {
-//         const file = e.target.files[0];
-        
-//         if (file) {
-//             const reader = new FileReader();
-            
-//             reader.onload = function(event) {
-//                 previewArte.src = event.target.result;
-//                 previewArte.style.display = 'flex';
-//             };
-//             labelArte.textContent='imagem selecionada'
-            
-//             reader.readAsDataURL(file);
-//         }else{
-//             previewArte.style.display='none';
-//             previewArte.src='';
-//         }
-//     });
-// });
 
