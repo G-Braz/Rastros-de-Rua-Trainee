05973cd1b63133b6025f4be3cde3236f95510928
Caio Ribeiro <caioalvribeiro@gmail.com>
2025-05-14 20:30:29 -0300

    adiciona filtro funcional

--- public/js/listaPosts.js
+++ public/js/listaPosts.js
@@ -1,3 +1,14 @@
 function apagarTexto(idInput){
     document.getElementById(idInput).value="";
 }
+const tela = document.querySelector('#tela');
+
+function abrirFiltro(idFiltro){
+    document.getElementById(idFiltro).style.display="flex";
+    tela.style.display="block";
+    
+}
+function fecharFiltro(idFiltro){
+    document.getElementById(idFiltro).style.display="none";
+    tela.style.display="none";
+}
