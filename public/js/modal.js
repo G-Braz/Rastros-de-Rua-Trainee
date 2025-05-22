38f880c307e5efbb0f391dbbabbf90a547bae398
Mickaell-Oliveira <mickaell.oliveira@estudante.ufjf.br>
2025-05-21 22:34:19 -0300

    Ajustando os modais de acordo com as solicitações dos clientes+ codigo em js para tamanho dos campos + ajustes no codigo

--- public/js/modal.js
+++ public/js/modal.js
@@ -4,6 +4,10 @@ function abrirModal(idFundo, idModal){ // Abre o modal
     setTimeout(() => {
     document.getElementById(idFundo).classList.add('opacidade'); 
     }, 10);
+
+    if(idModal==='modalEditar' || idModal==='modalVisualizar'){ //ajusta o tamanho dos campos descrição e materiais ao abrir o modal editar
+      ajustarTextAreasInicialmente();
+    }
 }
 function fecharModal(idFundo, idModal){ // Fecha o modal
     document.getElementById(idFundo).classList.remove('opacidade');
@@ -36,3 +40,25 @@ function salvarAlteracoes() {
     fecharModal('fundoEditar','modalEditar'); // Fecha o modal
 }
 
+function autoResize(el) { // função para ajustar o tamanho
+  el.style.height = 'auto';
+  el.style.height = (el.scrollHeight) + 'px';
+}
+
+function ajustarTextAreasInicialmente() { // executa o reajuste de tamanho
+  const textAreas = [
+    document.getElementById('textAreaDescricao'),
+    document.getElementById('textAreaMateriais'),
+    document.getElementById('')
+  ];
+  
+  textAreas.forEach(textarea => {
+    if (textarea) {
+      // Ajusta inicialmente
+      autoResize(textarea);
+      
+      // Futuras alterações
+      textarea.addEventListener('input', () => autoResize(textarea));
+    }
+  });
+}
