4c69fb126fec899390dcdbdd8c17828a8e76b053
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2025-05-29 00:08:45 -0300

    Finaliza lógica da paginação

--- app/Controllers/UsuariosController.php
+++ app/Controllers/UsuariosController.php
@@ -10,8 +10,27 @@ class UsuariosController
 
     public function index()
     {
-        $usuarios = App::get('database')->selectAll('usuarios');
-        return view('admin/Lista-de-Usuarios', compact('usuarios'));
+        $page = 1;
+        if(isset($_GET['paginacaoNumero']) && !empty($_GET['paginacaoNumero'])){
+                $page = intval($_GET['paginacaoNumero']);
+        }
+        if($page<=0){
+                return redirect('admin/Lista-de-Usuarios');
+        }
+        $itens_page = 5;
+        $inicio = $itens_page * $page - $itens_page;
+        $rows_count = App::get('database')->countAll('usuarios');
+
+        if($inicio > $rows_count){
+                return redirect('admin/Lista-de-Usuarios');
+        }
+        $usuarios = App::get('database')->selectAll('usuarios', $inicio, $itens_page);
+        $total_pages = ceil($rows_count/$itens_page);
+        return view('admin/Lista-de-Usuarios', [
+                'usuarios' => $usuarios,
+                'page' => $page,
+                'total_pages' => $total_pages
+        ]);
     }
 
     public function criar_usuario()
--- app/routes.php
+++ app/routes.php
@@ -5,7 +5,6 @@ use App\Controllers\ExampleController;
 use App\Core\Router;
 
 $router->get('', 'ExampleController@index');
-$router->get('usuarios', 'PaginacaoController@index');
 $router->get('usuarios', 'UsuariosController@index');
 $router->post('usuarios/criar_usuario', 'UsuariosController@criar_usuario');
 $router->post('usuarios/editar_usuario', 'UsuariosController@editar_usuario');
